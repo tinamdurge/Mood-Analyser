ae324c9146f5afd15170a3be90bf389c608aebad
Tinam Durge <tinamdurge02@gmail.com>
2023-12-10 22:43:08 +0530

    Added: Ability to handle exceptions

--- src/MoodException.java
+++ src/MoodException.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class MoodException {
+public class MoodException extends Exception {
+    /*
+     * @name: MoodAnalysisException
+     * @desc: Constructor for MoodAnalysisException class
+     * @param: message: String
+     */
+    public MoodException(String message, MoodErrors nullMessage) {
+        super(message);
+    }
 }
